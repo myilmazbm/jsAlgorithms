4e1024ed03ecc007567e9f5c4558f05b672bc665
Muhammed Yılmaz <39902568+myilmazbm@users.noreply.github.com>
2022-09-07 16:40:17 +0300

    Add files via upload

---
+++ easy.js
@@ -0,0 +1,12 @@
+//import { userValidation } from "./easy/1_userValidation.js";
+//import { longestWord } from "./easy/2_longestWord.js";
+// import { firstFactorial } from "./easy/3_firstFactorial.js";
+// console.log( firstFactorial(5) );
+// import { firstReverse } from "./easy/4_firstReverse.js";
+// console.log( firstReverse("xyz") );
+// import { findIntersection } from "./easy/5_findIntersection.js";
+// console.log(findIntersection(["3,7","1,2,4,13,15"]));
+// import { questionMark } from "./easy/6_questionMark.js";
+// console.log(questionMark("acc?7??sss?3rr1??????5"))
+import { letterChange } from "./easy/7_letterChange.js";
+console.log(letterChange("asdfasdfzzzyyy"))
