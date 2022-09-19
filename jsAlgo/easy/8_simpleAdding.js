321644d90191620a058528f958d5a79277584021
Muhammed Yılmaz <39902568+myilmazbm@users.noreply.github.com>
2022-09-19 15:09:47 +0300

    Add files via upload

---
+++ jsAlgo/easy/8_simpleAdding.js
@@ -0,0 +1,8 @@
+export const simpleAdding = (num) => {
+    // let sum = 0
+    // for (let index = 0; index < num; index++) {
+    //     sum += index
+    // }
+    // return sum
+    return num*(num+1)/2
+}
